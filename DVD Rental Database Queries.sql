0f20c276ddb37510fc3fe6aef3ca89bfd77530ef
Olga <141760577+olga-erkiert@users.noreply.github.com>
2024-06-09 16:19:12 +0200

    Update DVD Rental Database Queries.sql
    Add query 2

--- DVD Rental Database Queries.sql
+++ DVD Rental Database Queries.sql
@@ -1,11 +1,11 @@
 /*DVD Rental Database (link: https://www.postgresqltutorial.com/postgresql-getting-started/postgresql-sample-database/) */
 
 
-/*Prepare list of customers and newsletter message about promotion for the next customer's rental
+/* QUERY 1
+Prepare list of customers and newsletter message about promotion for the next customer's rental
 when customer rented more then 30 films he gets 30% discount
 when customer rented between 20-29 films he gets 20% discount
 when customer rented between 15-19 films he gets 10% discount
-
 */
 
 SELECT customer.customer_id, first_name, last_name, email, COUNT(inventory_id) AS total_rental, 
@@ -22,3 +22,27 @@ LEFT OUTER JOIN rental
 ON customer.customer_id = rental.customer_id
 GROUP BY customer.customer_id
 ORDER BY COUNT(inventory_id) DESC;
+
+
+/* QUERY 2
+Prepare a list of all customers who have ever rented a movie more than once.
+*/
+
+WITH customers_rentals AS (
+	SELECT f.film_id film, f.title title, c.customer_id customer, 
+		CONCAT(c.first_name, ' ', c.last_name) customer_name
+	FROM film AS f
+	LEFT JOIN inventory AS i
+	ON f.film_id = i.film_id
+	LEFT JOIN rental AS r
+	ON i.inventory_id = r.inventory_id
+	LEFT JOIN payment AS p
+	ON r.rental_id = p.rental_id
+	LEFT JOIN customer c
+	ON r.customer_id = c.customer_id
+)
+SELECT title, customer_name, COUNT (*) AS rental_count 
+FROM customers_rentals
+GROUP BY title, customer_name
+HAVING COUNT(*) > 1
+ORDER BY rental_count DESC;
